89687db07ef000eb1b46871ca8c74fb39cf1f672
danielt105 <danielt105@gmail.com>
2015-11-19 22:49:42 -0800

    still doesnt work ...

--- routes/api/ministry.js
+++ routes/api/ministry.js
@@ -45,19 +45,43 @@ exports.create = function(req, res) {
 	});
 }
 
+function addQueryParam(curString, targetCondition) {
+	if (targetCondition != undefined) {
+		curString = curString + ", " + targetCondition;
+		console.log("added " + curString);
+	}
+	return curString;
+}
+
+// Usage:
+//   var data = { 'first name': 'George', 'last name': 'Jetson', 'age': 110 };
+//   var querystring = EncodeQueryData(data);
+// 
+function EncodeQueryData(data)
+{
+   var ret = [];
+   for (var d in data)
+      ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+   return ret.join("&");
+}
+
 //query for a ministry
 exports.specialfind = function(req, res) {
-	console.log("url is  " + req.originalUrl +  " 1)" + req.query.condition + " order)" + req.query.order + " lim)" + req.query.limit);
-
-	var lim = req.query.limit;
-	if (lim == undefined) {
-		console.log("haha caught this mother fucker");
-	}
+	console.log("url is  " + req.originalUrl +  " 1)" + req.query.condition + " order)" + req.query.order);
 	
 
-	var query = Ministry.model.find( { name : "Destino" } );
+	var queryStr = req.query.condition;
+	var lim = (req.query.limit == undefined) ? 0 : req.query.limit;
+	//still need to parse select somehow
 
-	//can use query.select('<field name>') to filter
+	console.log("trying to find " + queryStr);
+	console.log(("{ name : \"Destino\" }" === queryStr) + " " + ("{ name : \"Destino\" }" == queryStr));
+
+		var poo = "{ name : \"Destino\" }";
+
+	var query = Ministry.model.find( /*{ name : "Destino" }*/ poo).limit(lim);
+
+
 
 	query.exec(function(err, items) {
 		if (err) return res.apiError('database error', err);
